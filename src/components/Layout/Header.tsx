0892085eb4a1fa04bd106b5276b07c753016ba64
Goemon5 <119960019+Goemon5@users.noreply.github.com>
2024-07-17 20:25:33 +0900

    Revert "Remove unnecessary code from header to optimize performance"
    This reverts commit d32414ba8b158be8daad7fbb1eb8028022805d70.

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -5,6 +5,14 @@ const Header = () => {
     <>
       <header>
         <h1 className="title">サービス名</h1>
+        <h1 className="links">
+          <a href="#" className="links_home">
+            ホーム
+          </a>
+          <a href="#" className="links_home">
+            投稿
+          </a>
+        </h1>
       </header>
     </>
   );
